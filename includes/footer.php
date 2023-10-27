8ef2e8de0ad029a213eb5e49c3955a50c4dbc41b
Pushpraj Dwivedi <pushprajdwivedi001@g.ail.com>
2023-10-27 10:58:54 +0530

    initial - working

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<footer class="footer"> <img src="images/logo.png" alt="Image">
+    <ul class="social-media">
+        <li><a href="#">FB--POOPOSIET</a></li>
+        <li><a href="#">TW--POOPOSIET</a></li>
+        <li><a href="#">YT--POOPOSIET</a></li>
+        <li><a href="#">BE--POOPOSIET</a></li>
+    </ul>
+    <h4>Music Unleashed, Creativity Unbound</h4>
+    <h2>Have an idea or project? Let's talk</h2>
+    <a href="#" class="btn-contact"><span data-hover="LET'S BE IN TOUCH">LET'S BE IN TOUCH</span></a>
+    <div class="footer-bar"> <span class="pull-left">© 2021 Anchor - All rights reserved.</span> <span class="pull-right">Site created by <a href="#">Themezinho</a></span> </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<header class="header">
+    <div class="logo"><img src="images/logo.png" alt="Image"></div>
+    <!-- end logo -->
+    <!-- <span class="phone">t: +380 83 857 5 577</span> -->
+    <div class="hamburger" id="hamburger">
+        <div class="hamburger__line hamburger__line--01">
+            <div class="hamburger__line-in hamburger__line-in--01"></div>
+        </div>
+        <div class="hamburger__line hamburger__line--02">
+            <div class="hamburger__line-in hamburger__line-in--02"></div>
+        </div>
+        <div class="hamburger__line hamburger__line--03">
+            <div class="hamburger__line-in hamburger__line-in--03"></div>
+        </div>
+        <div class="hamburger__line hamburger__line--cross01">
+            <div class="hamburger__line-in hamburger__line-in--cross01"></div>
+        </div>
+        <div class="hamburger__line hamburger__line--cross02">
+            <div class="hamburger__line-in hamburger__line-in--cross02"></div>
+        </div>
+    </div>
+    <!-- end hamburger -->
+    <div class="equalizer"> <span></span> <span></span> <span></span> <span></span> </div>
+    <!-- end equalizer -->
+
+    <ul class="language">
+        <!-- <li><a href="#">EN</a></li>
+        <li><a href="#">RU</a></li> -->
+    </ul>
+</header>
